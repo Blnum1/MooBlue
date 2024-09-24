b021234ddf2fdda9f7c4df5b33cf80fcc4a84285
Blnum1 <jirayut.pr@ku.th>
2024-09-24 16:07:25 +0700

    MongoDB-Success

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,13 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+
 import express from "express";
 import cors from "cors";
-import { sample_Porks, sample_tags, sample_users } from "./data";
-import jwt from "jsonwebtoken";
+import porkRouter from './routers/pork.router';
+import userRouter from './routers/user.router';
+import { dbConnect } from './configs/database.config';
+dbConnect();
 
 const app = express();
 app.use(express.json());
@@ -10,57 +16,8 @@ app.use(cors({
     origin:["http://localhost:4200"]
 }));
 
-app.get("/api/porks", (req,res) => {
-    res.send(sample_Porks);
-})
-
-app.get("/api/porks/search/:serchTerm", (req, res) => {
-    const searchTerm = req.params.serchTerm;
-    const porks = sample_Porks
-    .filter(pork => pork.name.toLowerCase()
-    .includes(searchTerm.toLowerCase()));
-    res.send(porks);
-})
-
-app.get("/api/porks/tags", (req, res) => {
-    res.send(sample_tags);
-})
-
-app.get("/api/porks/tag/:tagName", (req, res) => {
-    const tagName = req.params.tagName;
-    const porks = sample_Porks
-    .filter(pork => pork.tags?.includes(tagName));
-    res.send(porks);
-})
-
-app.get("/api/porks/:porkId", (req, res) => {
-    const porkId = req.params.porkId;
-    const pork = sample_Porks.find(pork => pork.id == porkId);
-    res.send(pork);
-})
-
-app.post("/api/users/login", (req,res) => {
-    const {email, password} = req.body;
-    const user = sample_users.find(user => user.email === email &&
-        user.password === password);
-
-        if(user){
-            res.send(generateTokenResponse(user));
-        }else{
-            res.status(400).send("User name or password is not valid!");
-        }
-    })
-
-const generateTokenResponse = (user:any)=>{
-    const token = jwt.sign({
-        email:user.email, isAdmin:user.isAdmin
-    },"SomeRandomText", {
-        expiresIn:"30d"
-    });
-
-    user.token = token;
-    return user;
-}
+app.use("/api/porks", porkRouter);
+app.use("/api/users", userRouter);
 
 const port = 5000;
 app.listen(port, () => {
--- fontend/src/app/login/login.component.ts
+++ fontend/src/app/login/login.component.ts
@@ -1,31 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { UserService } from '../services/user.service';
-import { ActivatedRoute, Route, Router } from '@angular/router';
-import { InputContainerComponent } from '../partials/input-container/input-container.component';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
-  
 })
 export class LoginComponent implements OnInit {
-
   loginForm!: FormGroup;
   isSubmitted = false;
   returnUrl = '';
-  constructor(private formBuilder: FormBuilder
-    , private userService:UserService, 
-    private activatedRoute:ActivatedRoute,
-    private router:Router) {}
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private userService: UserService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.loginForm = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
-      password: ['', Validators.required]
+      password: ['', Validators.required],
     });
-
 
     this.returnUrl = this.activatedRoute.snapshot.queryParams.returnUrl;
   }
@@ -33,14 +32,15 @@ export class LoginComponent implements OnInit {
   get fc() {
     return this.loginForm.controls;
   }
-  
+
   submit() {
     this.isSubmitted = true;
     if (this.loginForm.invalid) return;
 
-      this.userService.login({email:this.fc.email.value, 
-        password: this.fc.password.value}).subscribe(() => {
-          this.router.navigateByUrl(this.returnUrl);
-        });
+    this.userService
+      .login({ email: this.fc.email.value, password: this.fc.password.value })
+      .subscribe(() => {
+        this.router.navigateByUrl(this.returnUrl);
+      });
   }
 }
